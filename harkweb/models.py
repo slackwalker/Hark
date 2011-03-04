a99e57d6cf95a281765f697928de45244d6baa39
Todd Graham <sleeper@deepshell.net>
2011-03-04 06:45:51 +0000

    made some changes to the templates and removed filename from the song model

--- harkweb/models.py
+++ harkweb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from hark.harkweb.templatetags.harkweb_extras import harkify
 
 class Artist(models.Model):
     name = models.CharField(max_length=64)
@@ -17,12 +18,18 @@ class Album(models.Model):
     def fullname(self):
         return '%s - %s' % (self.artist, self.name)
 
+    def urlpath(self):
+        return '%s/%s/' % (harkify(self.artist), harkify(self.name))
+        
+
 class Song(models.Model):
     name = models.CharField(max_length=64)
-    filename = models.CharField(max_length=64)
     album = models.ForeignKey(Album)
     track = models.IntegerField()
 
     def __unicode__(self):
         return self.name
+
+    def urlname(self):
+        return '%02d_%s' % (self.track, harkify(self.name))
 
